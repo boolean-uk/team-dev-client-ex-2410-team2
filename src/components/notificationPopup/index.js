f94b3f765a7cee7c5c37fa88b1322c3faa25ee62
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-10-30 09:38:34 +0100

    Changed the notificationPopup so it can have no icon as well

--- src/components/notificationPopup/index.js
+++ src/components/notificationPopup/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './style.css';
 import CheckmarkIcon from '../../assets/icons/checkmarkIcon';
 
-const NotificationPopup = ({ message, actionText, onAction }) => {
+const NotificationPopup = ({ message, actionText, onAction, variant = 'success' }) => {
   return (
     <div className="snackbar">
-      <div className="snackbar-icon">
-        <CheckmarkIcon />
-      </div>
+      <div className="snackbar-icon">{variant === 'success' && <CheckmarkIcon />}</div>
       <span className="snackbar-message">{message}</span>
       {actionText && (
         <button onClick={onAction} className="snackbar-action">
